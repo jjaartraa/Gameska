30cb9318a556ffe9242c5d36ff22958dd1b96e4b
Jaroslav Lauschmann <jjaartraa@gmail.com>
2023-02-26 00:19:07 +0100

    Add option to choose game from Console instead of manually editing Main()

--- Gameska/Program.cs
+++ Gameska/Program.cs
@@ -6,17 +6,53 @@ namespace Gameska
 {
     class Program
     {
-        /*
-        // Gameska Main
+        static SnakeGame Snek;
         static void Main(string[] args)
         {
-            
+            string userInput;
+            do
+            {
+                Console.WriteLine("Please select Game: snake / maze." + Environment.NewLine + "Or type exit to exit");
+                userInput = Console.ReadLine();
+
+                switch (userInput.ToUpper())
+                {
+                    case "SNAKE":
+                        Snake();
+                        break;
+                    case "MAZE":
+                        Maze();
+                        break;
+                    case "EXIT":
+                        break;
+                    default:
+                        Console.WriteLine("Sorry I don't see this as an option.");
+                        break;
+                }
+            } while (userInput.ToUpper() != "EXIT");
+        }
+
+        static void Snake()
+        {
+            Snek = new SnakeGame(1,20,20);
             ConsoleKey lastkey = new ConsoleKey();
-            
+
+            Timer t = new Timer(TimerCallback, null, 500, 200);
+            do
+            {
+                lastkey = Console.ReadKey().Key;
+                Snek.SetDirection(lastkey.ToString());
+            } while (lastkey != ConsoleKey.Escape);
+        }
+
+        static void Maze()
+        {
+            ConsoleKey lastkey = new ConsoleKey();
+
             Console.WriteLine("Press Any Key To Start");
             Config cfg = new Config();
             cfg.LoadConfig();
-            Display display = new Display(cfg.map, cfg.ViewResolution, cfg.PPX, cfg.PPY); 
+            Display display = new Display(cfg.map, cfg.ViewResolution, cfg.PPX, cfg.PPY);
 
             do
             {
@@ -24,32 +60,16 @@ namespace Gameska
                 display.PlayerMove(lastkey.ToString());
                 Console.SetCursorPosition(0, 0);
                 Console.WriteLine(display.Refresh());
-                
+
             } while (lastkey != ConsoleKey.Escape);
         }
-        */
-        // Snake Game Main
-        
-        static SnakeGame Snek = new SnakeGame(1, 20, 20);
-        static void Main(string[] args)
-        {
 
-            ConsoleKey lastkey = new ConsoleKey();
-
-            Timer t = new Timer(TimerCallback, null, 500, 500);
-            do
-            {
-                lastkey = Console.ReadKey().Key;
-                Snek.SetDirection(lastkey.ToString());
-            } while (lastkey != ConsoleKey.Escape);
-        }
-        
         private static void TimerCallback(Object o)
         {
             Console.SetCursorPosition(0, 0);
             Console.WriteLine(Snek.MoveSnake());
             GC.Collect();
         }
-        
+
     }
 }
